b1796594d920884ed753c115352336834d8d681a
Darwin Perdomo <ZeroBl@hotmail.com>
2022-04-22 23:38:18 -0400

    feat(front): Added Laurels section and his styles

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { images,data } from '../../constants';
+import { SubHeading } from '../../components';
 
 import './Laurels.css';
 
-const Laurels = () => (
-  <div>
-    Laurels
+const AwardCard = ({ award: {imgUrl, title, subtitle} }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color:"#FCCA87" }}>{title}</p>
+      <p className="p__cormorant">{subtitle}</p>
+    </div>
   </div>
+);
+
+const Laurels = () => (
+  <section className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & Reconition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => <AwardCard award={award} key={award.title}/>)}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels" /> 
+    </div>
+    
+  </section>
 );
 
 export default Laurels;
